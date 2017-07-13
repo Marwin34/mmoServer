88d5d577cf0c73a70c345ca3e2a7e985bc0a6a78
Marwin34 <marcinwojsik@wp.pl>
2017-07-13 16:19:50 +0200

    13.07.2017

---
+++ mmoServer/enemy.cpp
@@ -0,0 +1,73 @@
+#include "enemy.h"
+
+Enemy::Enemy(){
+	x = 0;
+	y = 0;
+	maxHp = 0;
+	currHp = maxHp;
+	damage = 0;
+	range = 0;
+	respawnTime = sf::Time::Zero;
+	attack = false;
+}
+
+Enemy::~Enemy(){
+
+}
+
+void Enemy::init(std::string name, float startX, float startY){
+	if (name == "Orc"){
+		x = startX;
+		y = startY;
+		maxHp = 100;
+		currHp = maxHp;
+		respawnTime = sf::seconds(5);
+		range = 16;
+	}
+}
+
+void Enemy::update(){
+	
+}
+
+void Enemy::harm(int damage){
+	currHp -= damage;
+}
+
+float Enemy::getRange(){
+	return range;
+}
+
+float Enemy::getX(){
+	return x;
+}
+
+float Enemy::getY(){
+	return y;
+}
+
+bool Enemy::isAttacking(){
+	return attack;
+}
+
+sf::Time Enemy::getRespawnTime(){
+	return respawnTime;
+}
+
+sf::Packet& operator <<(sf::Packet& packet, const Enemy& enemy)
+{
+	return packet << enemy.id << enemy.x << enemy.y << enemy.attack << enemy.maxHp << enemy.currHp;
+}
+
+std::vector<Enemy> enemiesInit(std::string mapName){
+	std::vector<Enemy> tmp;
+	tmp.resize(0);
+	if (mapName == "starting"){
+
+		Enemy tmpEnemy;
+		tmpEnemy.init("Orc", 200, 200);
+		tmp.push_back(tmpEnemy);
+	}
+
+	return tmp;
+}
---
+++ mmoServer/enemy.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <SFML/Network.hpp>
+
+class Enemy{
+	float x, y;
+	float spdX, spdY;
+	int maxHp;
+	int currHp;
+	int damage;
+	int id;
+	sf::Time respawnTime;
+	float range;
+	bool attack;
+
+	friend sf::Packet& operator <<(sf::Packet&, const Enemy&); // Send operator.
+public:
+	Enemy();
+	~Enemy();
+	void init(std::string, float, float);
+	void update();
+	void harm(int);
+	float getRange();
+	float getX();
+	float getY();
+	bool isAttacking();
+	sf::Time getRespawnTime();
+};
+
+std::vector<Enemy> enemiesInit(std::string);
